56a9da8ea40a90fc278c22eb60f1b634915834de
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-10-03 18:23:25 +0200

    system_prop.mk: Modified ro.lineage.version

--- system_prop.mk
+++ system_prop.mk
@@ -1,5 +1,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.lineage.version=Ksawlii-LineageOS-18.1-gprimeltetfnvzw
+  ro.lineage.version=Ksawlii-LineageOS-18.1-gprimelteusc
 # CABL
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
